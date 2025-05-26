efff190a81a1950b53559af6175fedd1ba3d82ab
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-26 16:22:12 -0500

    Rename atomic source files and update CMakeLists to reflect new paths

--- atomic.c
+++ atomic.c
@@ -7,4 +7,4 @@
  * This is free software, and you are welcome to redistribute it
  * under certain conditions; type `show c' for details.
 */
-#include "atomic.h"
+#include "include/fluent/atomic/atomic.h"
